14d47701cd2558a2ffec9a55ef0a94d57704248a
pobiq <hoon2207@naver.com>
2025-12-24 11:28:30 +0900

    요청경로 설정, 접속IP 수정

--- BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
+++ BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smhrd.web.entity.Board;
 import com.smhrd.web.service.BoardService;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
+@RequestMapping("/api/board")
+@CrossOrigin(origins = "http://10.2.1.6", allowedHeaders = "*")
+//@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
 public class BoardRestController {
 	
 	@Autowired
